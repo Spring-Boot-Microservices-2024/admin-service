3bf66a7f000922a20db4b49b807ca516298614f9
Svitlana-Marchenko <sveta25022004@gmail.com>
2024-10-17 20:56:43 +0300

    feat: add security

---
+++ src/main/java/org/naukma/adminservice/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package org.naukma.adminservice;
+
+import de.codecentric.boot.admin.server.web.client.HttpHeadersProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Value("${auth.header}")
+    private String authHeader;
+
+    @Value("${auth.key}")
+    private String authKey;
+
+    @Bean
+    public HttpHeadersProvider customHttpHeadersProvider() {
+        return (instance) -> {
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add(authHeader, authKey);
+            return httpHeaders;
+        };
+    }
+}
